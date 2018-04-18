5390f1d117e4fc22b2d6a07585663a26fbced923
Mauricio Guzmán Salazar <mauricio.guzman.salazar@gmail.com>
2018-04-18 14:53:21 -0500

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,37 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.sendNotification = functions.database.ref('/notifications/chats/{pushId}').onCreate((event) => {
+  // obtains the new notification
+  const notification = event._data;
+
+  //extracts some data from the notification
+  const deviceToSend = notification.receiver;
+  const displayMessage = notification.message;
+  const sender = notification.sender;
+
+  const payload = {
+    data: {
+      title: displayMessage,
+      body: "de " + sender
+    }
+  };
+
+  admin.messaging().sendToDevice(deviceToSend, payload)
+    .then(response => console.log("Succesfully sent message:", response))
+    .then(event.ref.remove()
+            .then(ok => console.log("Successfully deleted node ", ok))
+            .catch(error => console.log("Error deleting node", error))
+          )
+    .catch(error => console.log("Error sending message:", error));
+
+  return 0;
+});
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
